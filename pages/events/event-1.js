bdc1adf04432524d7f365f2a5e4c294236b88ce4
shoaibkhan188626 <82094195+shoaibkhan188626@users.noreply.github.com>
2022-12-18 00:23:40 +0530

    App deployed kindly suggest if there is some room for emprovement

---
+++ pages/events/event-1.js
@@ -0,0 +1,6 @@
+const Page = ()=>{
+    return(
+        <h1>Event Page</h1>
+    )
+};
+export default Page;
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+export const Footer=()=>{
+    return(
+        <footer>
+            <p>
+                Practice Project Done By Shoaib Khan Using React and Next
+            </p>
+        </footer>
+    )
+}
